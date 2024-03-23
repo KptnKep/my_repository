a92661949af6ac02cc56adaea15143369b580213
KptnKep <4530083@mail.ru>
2024-03-23 08:40:46 +0700

    Убрал проверку на букву и больше 3 элементов

--- bird.py
+++ bird.py
@@ -187,18 +187,17 @@ def customcards_on_open(hashMap, _files=None, _data=None):
 
 
 def save_bird(hashMap, _files=None, _data=None):
-    if hashMap.get("bird").isalpha() and len(hashMap.get("bird")) > 3 and hashMap.get("bloom").isalpha() and len(hashMap.get("bloom")) > 3:
-        i = len(c) + 1
-        data = {
-            "key": str(i),
-            "descr": "Pos. " + str(i),
-            "string1": hashMap.get("bird"),
-            "string2": hashMap.get("bloom")
-        }
 
-        c.append(data)
-        hashMap.put("toast", "Добавленно" )
-    else:
-        hashMap.put("toast", "Внесите название и цвет птицы!")
+    i = len(c) + 1
+    data = {
+        "key": str(i),
+        "descr": "Pos. " + str(i),
+        "string1": hashMap.get("bird"),
+        "string2": hashMap.get("bloom")
+    }
+
+    c.append(data)
+    hashMap.put("toast", "Добавленно" )
+
 
     return hashMap
